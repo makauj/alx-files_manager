83d1920ad17dc3325a9353465ede6bafff682918
makau6 <makauwanyoike@gmail.com>
2025-06-21 13:56:25 +0300

    refactor(controller): improve error handling and password hashing in UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,41 +1,39 @@
-/* eslint-disable */
+const { ObjectId } = require('mongodb');
 const crypto = require('crypto');
-const dbClient = require('./utils/db');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
-    }
-    if (!password) {
-      return res.status(400).json({ error: 'Missong password' });
-    }
+    if (!email) return res.status(400).json({ error: 'Missing email' });
 
-    try {
-      const usersCollection = dbClient.db.collection('users');
-      const existingUser = await usersCollection.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-      const hashedPassword = (
-        crypto.createHash('sha256')
-          .update(password)
-          .digest('hex')
-      );
-      const result = await usersCollection.insertOne({
-        email,
-        password: hashedPassword,
-      });
-      return res.status(201).json({
-        id: result.insertedId,
-        email,
-      });
-    } catch (error) {
-      console.error('Error in UsersController.postNew:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+    const existing = await dbClient.db.collection('users').findOne({ email });
+    if (existing) return res.status(400).json({ error: 'Already exist' });
+
+    const hashedPassword = (crypto.createHash('sha1')
+      .update(password).digest('hex'));
+
+    const result = await (dbClient.db.collection('users')
+      .insertOne({ email, password: hashedPassword }));
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const user = await (dbClient.db.collection('users')
+      .findOne({ _id: new ObjectId(userId) }));
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
