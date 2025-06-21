c41dc2236b75a7af8b496dfcf72d54c972cee43f
makau6 <makauwanyoike@gmail.com>
2025-06-21 13:36:06 +0300

    feat(routes): add AuthController routes for connect and disconnect

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-
+const AuthController = require('../controllers/Authcontroller')
 const router = express.Router();
 
 // Route: GET /status
@@ -11,5 +11,8 @@ router.get('/status', AppController.getStatus);
 // Route: GET /stats
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.createUser);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
