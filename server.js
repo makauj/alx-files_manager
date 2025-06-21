f13c655f4efee9ebba5d52a8c79f48b31f2a3695
makau6 <makauwanyoike@gmail.com>
2025-06-21 12:58:05 +0300

    feat(server): set up Express server with routing and environment configuration

---
+++ server.js
@@ -0,0 +1,13 @@
+/* eslint-disable*/
+const express = require('express');
+const routes = require('./routes/index');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(' /', routes);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
